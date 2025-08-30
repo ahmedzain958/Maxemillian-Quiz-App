dca402d2b383b1d2cc4928f48e2150bd49274741
Ahmed Zain <ahmed.zain@dxc.com>
2025-08-30 16:37:18 +0300

    lift out

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -10,7 +12,8 @@ class HomeScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         /*takes as minimal vertical space as needed */
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+          Image.asset('assets/images/quiz-logo.png', width: 300,
+          color: Color.fromARGB(150, 255, 255, 255)),
           const SizedBox(height: 80),
           Text(
             'Learn Flutter the fun way!',
@@ -18,8 +21,11 @@ class HomeScreen extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: () {
+              
+            },
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: Icon(Icons.arrow_right_alt),
             label: const Text('Start Quiz'),
           ),
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
-import 'package:advanced_basics_maxemellian/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:advanced_basics_maxemellian/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: HomeScreen(),
-        ),
-      ),
-    ),
+    const Quiz()
   );
 }
